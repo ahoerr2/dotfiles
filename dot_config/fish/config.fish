87a68b18b80a2e5c6ac80706f09da3a841395dcf
Alex Hoerr <alex.hoerr@precisionplanting.com>
2024-02-28 09:15:51 -0600

    Initial Commit

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,24 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Aliases
+alias cat='bat'
+alias gpull='git pull;git submodule update --init --recursive'
+alias gs='git status'
+alias gf='git fetch'
+alias gc='git checkout'
+alias gcb 'git checkout -b'
+alias lg='lazygit'
+alias vim='nvim'
+alias grebase='git rebase -i'
+alias gfpush="git push --force-with-lease"
+alias pip="pip3"
+alias python="python3"
+alias ...="cd ../.."
+alias ....="cd ../../.."
+set -gx PATH $PATH $HOME/.bin
+eval (/opt/homebrew/bin/brew shellenv)
+
+starship init fish | source
+
